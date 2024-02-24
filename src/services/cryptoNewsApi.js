2d91544bd3b04653980728be7275e22cb6a77c2a
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-02-24 13:18:39 +0530

    fix(news): :alien: migrated from bing api to newsapi for news data
    Bing News is not working via rapid api for last 3 months now, so we will be using newsapi now

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,19 +1,25 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const cryptoNewsHeaders = {
-  'x-bingapis-sdk': 'true',
-  'x-rapidapi-key': 'd59e389365msh2bb9b8eea4ca914p1fad86jsn1c9968d8850b',
-  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
-};
+const createRequest = (url) => ({ url });
 
-const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+const apiKey = "d98fb8ff8f9946c7ae9042eaf89085a1"
 
 export const cryptoNewsApi = createApi({
-  reducerPath: 'cryptoNewsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://bing-news-search1.p.rapidapi.com' }),
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://newsapi.org/v2" }),
   endpoints: (builder) => ({
     getCryptoNews: builder.query({
-      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+      query: ({ newsCategory }) => {
+        let date = new Date();
+
+        let from = `${date.getFullYear()}-${date.getMonth() + 1}-${
+          date.getDate() - 1
+        }`;
+
+        return createRequest(
+          `/everything?q=${newsCategory}&from=${from}&sortBy=publishedAt&apiKey=${apiKey}`
+        );
+      },
     }),
   }),
 });
